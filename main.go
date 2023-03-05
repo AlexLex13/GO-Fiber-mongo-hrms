b53f27b7e0313e51efaeca67bef518c56adc04a7
Alexey <suxovarov.alexey@gmail.com>
2023-03-05 16:12:39 +0300

    Add func Connect()

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
-import "github.com/gofiber/fiber"
+import (
+	"context"
+	"time"
+
+	"github.com/gofiber/fiber"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 type MongoInstance struct {
 	Client
@@ -11,6 +18,25 @@ var mg MongoInstance
 
 const dbName = "fiber-hrms"
 const mongoURI = "mongodb://localhost:27017" + dbName
+
+func Connect() error {
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	db := client.Database(dbName)
+
+	if err != nil {
+		return err
+	}
+
+	mg = MongoInstance{
+		Client: client,
+		Db:     db,
+	}
+	return nil
+}
 
 func main() {
 	app := fiber.New()
